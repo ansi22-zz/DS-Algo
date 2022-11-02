d2cb4ace8986ad8524c5f5fc46f09517d47e0cf7
Ritika Verma <76901399+ansi22@users.noreply.github.com>
2022-11-02 23:52:51 +0530

    45

---
+++ Codeforces/1730/A.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<list>
+#include<set>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<bitset>
+#include<string>
+#include<unordered_set>
+#include<unordered_map>
+#include<cstdlib>
+#include<iomanip>
+
+
+using namespace std;
+#define int long long int
+
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+
+void solve(){
+     int n,c;
+     cin>>n>>c;
+     int a[n],ans=0;
+     map<int,int> mp;
+     for(int i=0;i<n;i++) {
+          cin>>a[i];
+          mp[a[i]]++;
+     }
+
+     for(auto x: mp) {
+          if(x.second>1&&x.second>c) ans+=c;
+          else ans+=x.second;
+     }
+     cout<<ans<<"\n";
+     
+}
+
+
+void pre(){
+   
+}
+
+int32_t main(){
+
+     #ifndef ONLINE_JUDGE
+     freopen("input.txt","r",stdin); 
+     freopen("output.txt","w",stdout);
+     #endif
+
+     ios_base::sync_with_stdio(false); 
+     cin.tie(NULL);
+
+     int t=1;
+     cin>>t;
+     // pre();
+     for(int tc=1;tc<=t;tc++)
+     solve();
+     return 0;
+}
