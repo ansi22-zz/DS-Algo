f9d0cbf853fc74e8f89ec85139039a129c42c6dd
Ritika Verma <76901399+ansi22@users.noreply.github.com>
2022-11-26 00:18:26 +0530

    49

--- Codeforces/1758/C.cpp
+++ Codeforces/1758/C.cpp
@@ -9,26 +9,23 @@ void solve() {
 	
 	if(n%x!=0) cout<<-1<<"\n";
 	else {
-		vector<int> a;
-		a.push_back(x);
-		for(int i=2;i<=n-1;i++) {
-			if(i==x) a.push_back(n);
-			else a.push_back(i);
+		int a[n+1];
+		for(int i=1;i<=n;i++) {
+			a[i]=i;
 		}
-		a.push_back(1);
-		int check=a[x-1];
-		int ind=x-1;
-		for(int i=x-1;i<n-1;i+=x) {
-			if(check%a[i]==0) {
-				swap(a[ind], a[i]);
-				ind=i;
+		a[1]=x;
+		a[n]=1;
+		
+		for(int i=2*x;i<=n;i+=x) {
+			if(n%i==0) {
+				a[x]=i;
+				x=i;
 			}
 		}
 		
-		for(auto x: a){
-			cout<<x<<" ";
+		for(int i=1;i<=n;i++) {
+			cout<<a[i]<<" ";
 		}
-		
 		cout<<"\n";
 	}
 }
